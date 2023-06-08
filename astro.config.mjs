21b75ccf90f535bbc50ecb2936c948163440c51d
Wooseop Kim <wooseop@protonmail.com>
2023-06-09 01:12:48 +0900

    Set `base`

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wooseopkim.github.io/repro",
+  base: "/repro",
   integrations: [image()]
 });
